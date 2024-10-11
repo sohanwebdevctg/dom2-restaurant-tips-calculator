164fc3108bc2d550a0e591bc5b8cf51ca54d6b8e
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-10-12 01:59:27 +0600

    create all calculate function and set data

--- js/app.js
+++ js/app.js
@@ -35,12 +35,42 @@ const decBtn = () => {
 // get calculated function
 const calculateBtn = () => {
 
-  // totalBillValue
-  let totalBill = document.getElementById('totalBill').value;
-  let totalBillValue = Number(totalBill);
-  // tipsValue
-  let tips = document.getElementById('tips').value;
-  let tipsValue = Number(tips);
+  // totalBillData
+  let totalBillData = document.getElementById('totalBillData').value;
+  let totalBillValue = Number(totalBillData);
+  // tipsData
+  let tipsData = document.getElementById('tipsData').value;
+  let tipsValue = Number(tipsData);
+
+  // totalBill
+  let totalBill = document.getElementById('totalBill');
+
+  // tips
+  let tips = document.getElementById('tips');
+  let tipsPercent = (totalBillValue * tipsValue) / 100;
+
+  // tax
+  let tax = document.getElementById('tax');
+  let taxPercent = (totalBillValue + 5) / 100;
+
+  // total amount
+  let total = document.getElementById('total');
+  let totalValue = (totalBillValue + tipsPercent + taxPercent) / personValue;
+
+  if(totalBillData !== "" && tipsValue !== ""){
+
+    // all calculate data
+    totalBill.innerText = parseInt(totalBillValue);
+    tips.innerText = parseInt(tipsPercent);
+    tax.innerText = parseInt(taxPercent);
+    total.innerText = parseInt(totalValue);
+
+    totalBillData.value = "";
+    tipsData.value = "";
+
+  }else{
+    alert('please enter amount and tips')
+  }
 
   
 }
